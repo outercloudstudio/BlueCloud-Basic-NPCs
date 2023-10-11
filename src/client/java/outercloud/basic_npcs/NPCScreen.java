4abd39777a67bb344f252d9c7feac9e8bfaf1a3d
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-10-11 01:15:42 -0500

    test with chat provider

--- src/client/java/outercloud/basic_npcs/NPCScreen.java
+++ src/client/java/outercloud/basic_npcs/NPCScreen.java
@@ -4,11 +4,14 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.screen.ChatInputSuggestor;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.gui.widget.TextWidget;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
+import net.minecraft.util.Colors;
 import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 import outercloud.basic_npcs.mixins.client.ScreenMixin;
@@ -17,12 +20,13 @@ import outercloud.basic_npcs.mixins.client.TextureManagerMixin;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class NPCScreen extends HandledScreen<NPCScreenHandler> {
     private ButtonWidget appearanceTabButton = ButtonWidget.builder(Text.of("Appearance"), widget -> {}).dimensions(0, 0, 70, 16).build();
     private TextFieldWidget texturePathWidget;
-    private ArrayList<ButtonWidget> texturePathAutoCompletionOptionWidgets = new ArrayList<>();
+    private ChatInputSuggestor texturePathInputSuggestor;
 
     private ButtonWidget behaviourTabButton = ButtonWidget.builder(Text.of("Behaviour"), widget -> {}).dimensions(70, 0, 70, 16).build();
 
@@ -59,26 +63,24 @@ public class NPCScreen extends HandledScreen<NPCScreenHandler> {
         });
 
         setInitialFocus(texturePathWidget);
+
+        texturePathInputSuggestor = new ChatInputSuggestor(client, this, this.texturePathWidget, this.textRenderer, true, true, 0, 7, false, Integer.MIN_VALUE);
+        texturePathInputSuggestor.setWindowActive(true);
+        texturePathInputSuggestor.refresh();
     }
 
     private Set<Identifier> getAllEntityTextures() {
         return ((TextureManagerMixin)MinecraftClient.getInstance().getTextureManager()).getResourceContainer().findAllResources("textures", identifier -> identifier.getPath().startsWith("textures/entity/")).keySet();
     }
 
+
     private void onTexturePathChanged(String path) {
-        texturePathAutoCompletionOptionWidgets.forEach(this::remove);
-        texturePathAutoCompletionOptionWidgets = new ArrayList<>();
+        texturePathInputSuggestor.refresh();
 
-        Stream<Identifier> autoCompletions = allEntityTextures.stream().filter(identifier -> identifier.toString().startsWith(path));
-
+//        Stream<Identifier> autoCompletions = allEntityTextures.stream().filter(identifier -> identifier.toString().startsWith(path));
+//
 //        autoCompletions.forEach(identifier -> {
-//            ButtonWidget widget = ButtonWidget.builder(Text.of(identifier.toString().substring(path.length())), buttonWidget -> {
-//                texturePathWidget.setText(identifier.toString());
-//            }).dimensions(0, 0, 236, 14).build();
-//
-//            addDrawableChild(widget);
-//
-//            texturePathAutoCompletionOptionWidgets.add(widget);
+//            texturePathAutoCompletionWidget.setMessage(Text.of(identifier.toString()));
 //        });
 
         updateNPC();
@@ -96,9 +98,19 @@ public class NPCScreen extends HandledScreen<NPCScreenHandler> {
             return true;
         }
 
+        if (texturePathInputSuggestor.keyPressed(keyCode, scanCode, modifiers)) return true;
+
         if(this.texturePathWidget.isActive()) return this.texturePathWidget.keyPressed(keyCode, scanCode, modifiers);
 
         return super.keyPressed(keyCode, scanCode, modifiers);
+    }
+
+    public boolean mouseScrolled(double mouseX, double mouseY, double horizontalAmount, double verticalAmount) {
+        return this.texturePathInputSuggestor.mouseScrolled(verticalAmount) ? true : super.mouseScrolled(mouseX, mouseY, horizontalAmount, verticalAmount);
+    }
+
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        return this.texturePathInputSuggestor.mouseClicked(mouseX, mouseY, button) ? true : super.mouseClicked(mouseX, mouseY, button);
     }
 
     @Override
@@ -113,6 +125,8 @@ public class NPCScreen extends HandledScreen<NPCScreenHandler> {
         }
 
         this.drawForeground(context, mouseX, mouseY);
+
+        this.texturePathInputSuggestor.render(context, mouseX, mouseY);
     }
 
     @Override
